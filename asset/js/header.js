d4c3eed19aea78ffa58cdbe6336636b76be13987
ngoc <nguyenvanngoc.elvis@gmail.com>
2022-10-09 15:05:55 +0700

    ok

--- asset/js/header.js
+++ asset/js/header.js
@@ -1,5 +1,6 @@
 const cartBtn = $('.cart');
 const closeCartBtn = $('.close');
+const cartClose = $('.cart-bg-close');
 const cartSection = $('.cart-section');
 function handleToggleCart() {
     cartSection.toggleClass('active');
@@ -9,12 +10,12 @@ cartBtn.click(handleToggleCart)
 
 closeCartBtn.click(handleToggleCart)
 
-
+cartClose.click(handleToggleCart)
 //mobile
 
 const mobileMenuBtn = $('.mobile-menu-btn');
 const closeMenuBtn = $('.mobile-menu-close');
-const mobileMenuContainer = $('.mobile-menu-container');
+
 
 function handleToggleMenu(){
     mobileMenuContainer.toggleClass('active');
